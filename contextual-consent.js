f8849fef7b1ce8bcf07d05053327414e2e9b250d
danielanderssonberlin <mail@danielandersson.de>
2025-09-15 14:08:51 +0200

    Create contextual-consent.js

---
+++ contextual-consent.js
@@ -0,0 +1,48 @@
+function removeOverlays() {
+    document.querySelectorAll('.cookie-overlay').forEach(function (overlay) {
+        overlay.remove();
+    });
+}
+document.addEventListener("DOMContentLoaded", function () {
+    const hasMarketingConsent = typeof OnetrustActiveGroups !== 'undefined' && OnetrustActiveGroups.includes(',4,');
+    if (hasMarketingConsent) {
+        removeOverlays();
+        return;
+    }
+
+    const blockedIframes = document.querySelectorAll("iframe.optanon-category-4");
+
+    blockedIframes.forEach(function (iframe) {
+        if (iframe.dataset.src) {
+            // Wrapper erstellen
+            const wrapper = document.createElement("div");
+            wrapper.className = "cookie-iframe-wrapper";
+
+            const overlay = document.createElement("div");
+            overlay.className = "cookie-overlay";
+            overlay.innerHTML = `
+  <p>Dieses Video wird von YouTube eingebunden und verwendet Marketing-Cookies, um personalisierte Inhalte und Werbung anzuzeigen.<br>
+    Bitte akzeptieren Sie Marketing-Cookies, um das Video direkt auf dieser Seite ansehen zu können..</p>
+  <button type="button">Marketing-Cookies akzeptieren</button>
+  `;
+
+            iframe.parentNode.insertBefore(wrapper, iframe);
+            wrapper.appendChild(iframe);
+            wrapper.appendChild(overlay);
+
+            // Button-Handler
+            overlay.querySelector("button").addEventListener("click", function () {
+
+                OneTrust.UpdateConsent("Category", "4:1")
+
+                document.querySelectorAll("iframe.optanon-category-4[data-src]").forEach(function (iframe) {
+                    iframe.src = iframe.dataset.src;
+                });
+                removeOverlays();
+
+            });
+        }
+    });
+});
+
+
